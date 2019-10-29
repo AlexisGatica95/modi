ed32cd40d9ccc7e8f843461c30503df2d5faa70e
alexio <alexisgatica95@gmail.com>
2019-10-29 01:13:48 -0300

    con la vista del registro ♥

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductoComponent } from './components/producto/producto.component';
+
+
+const routes: Routes = [
+  {path : 'home', component : HomeComponent},
+
+  {path : 'productos', component : ProductosComponent},
+  {path : 'producto/:id', component : ProductoComponent},
+  {path : 'registro', component : RegistroComponent},
+  {path : 'login', component : LoginComponent},
+  {path : '**', redirectTo : 'home'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
